90926f6b87b1e6c90bf9f9527506f4da9d8e5b10
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 21:53:54 +0800

    finish can_get_cartItems_test method

--- src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
@@ -1,10 +1,15 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.vo.Item;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CartTest {
     Scanner scanner;
@@ -17,8 +22,29 @@ public class CartTest {
 
     @Test
     public void can_get_CartItems_test(){
-        Scanner scanner = new Scanner();
-        ItemService itemServiceImple = new ItemServiceImple();
+//        Scanner scanner = new Scanner();
+//        ItemService itemServiceImple = new ItemServiceImple();
+        Scanner scanner = mock(Scanner.class);
+        List<String> inputs = new ArrayList<String>();
+        inputs.add("ITEM000001");
+        inputs.add("ITEM000001");
+        inputs.add("ITEM000001");
+        inputs.add("ITEM000001");
+        inputs.add("ITEM000001");
+        inputs.add("ITEM000003-2");
+        inputs.add("ITEM000005");
+        inputs.add("ITEM000005");
+        inputs.add("ITEM000005");
+
+        when(scanner.getInputs()).thenReturn(inputs);
+        ItemService itemServiceImple = mock(ItemServiceImple.class);
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("1","ITEM000001", "葡萄", "斤", 6.5, "水果"));
+        items.add(new Item("2","ITEM000002", "橙汁", "瓶", 3.5, "饮品"));
+        items.add(new Item("3","ITEM000003", "苹果", "斤", 4.0, "水果"));
+        items.add(new Item("4","ITEM000004", "羽毛球", "个", 1.0, "运动器材"));
+        items.add(new Item("5","ITEM000005", "可口可乐", "瓶", 5.0, "饮品"));
+        when(itemServiceImple.getItems()).thenReturn(items);
         Cart cart = new Cart(scanner, itemServiceImple);
         assertThat(cart.getCartItems().size()).isEqualTo(3);
     }
