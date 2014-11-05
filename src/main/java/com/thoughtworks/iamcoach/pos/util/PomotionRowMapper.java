e38e39efadbe3e693412b293a99e12740fbc3eae
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 21:43:06 +0800

    create PromotionRowMapper class

--- src/main/java/com/thoughtworks/iamcoach/pos/util/PomotionRowMapper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/PomotionRowMapper.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.iamcoach.pos.util;
 
 import com.thoughtworks.iamcoach.pos.vo.Promotion;
+import com.thoughtworks.iamcoach.pos.vo.PromotionFactory;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.*;
@@ -8,7 +9,11 @@ import java.sql.*;
 public class PomotionRowMapper implements RowMapper {
     @Override
     public Object mapRow(ResultSet resultSet, int i) throws SQLException {
-        Promotion promotion;
-        return null;
+        Promotion promotion = PromotionFactory.getPromotionByType(resultSet.getInt("type"));
+            promotion.setId(resultSet.getString("id"));
+            promotion.setType(resultSet.getInt("type"));
+            promotion.setProDesc(resultSet.getString("proDesc"));
+            promotion.setDiscount(resultSet.getDouble("discount"));
+        return promotion;
     }
 }
