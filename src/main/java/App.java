6e12256f942002521ee2e82bbc949037dfb2acaa
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 21:44:47 +0800

    modify main with Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
 import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImple;
+import com.thoughtworks.iamcoach.pos.dao.ItemDao;
+import com.thoughtworks.iamcoach.pos.dao.ItemDaoImple;
 import com.thoughtworks.iamcoach.pos.service.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,12 +12,13 @@ public class App {
 
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        SimpleJdbcTemplate simpleJdbcTemplate = (SimpleJdbcTemplate)context.getBean("simpleJdbcTemplate");
 
-        ItemService itemServiceImple = new ItemServiceImple();
+        ItemDao itemDaoImple = new ItemDaoImple(simpleJdbcTemplate);
+        ItemService itemServiceImple = new ItemServiceImple(itemDaoImple);
         Scanner scanner = new Scanner();
         Cart cart = new Cart(scanner, itemServiceImple);
 
-        SimpleJdbcTemplate simpleJdbcTemplate = (SimpleJdbcTemplate)context.getBean("simpleJdbcTemplate");
         CategoryDao categoryDaoImple = new CategoryDaoImple(simpleJdbcTemplate);
         CategoryService categoryServiceImple = new CategoryServiceImpl(categoryDaoImple);
         Printer printer = new Printer(cart, categoryServiceImple );
