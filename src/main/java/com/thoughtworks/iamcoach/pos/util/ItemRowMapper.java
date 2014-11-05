4f720d152469ded3726b39cf3707b05363fe78f5
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 21:19:57 +0800

    finish mapRow method in ItemRowMapper

--- src/main/java/com/thoughtworks/iamcoach/pos/util/ItemRowMapper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/ItemRowMapper.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.iamcoach.pos.util;
 
-/**
- * Created by zhangzhihui on 14-11-5.
- */
-public class ItemRowMapper {
+
+import com.thoughtworks.iamcoach.pos.vo.Item;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.*;
+
+public class ItemRowMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+        Item item = new Item();
+        item.setId(resultSet.getString("id"));
+        item.setBarcode(resultSet.getString("barcode"));
+        item.setName(resultSet.getString("name"));
+        item.setUnit(resultSet.getString("unit"));
+        item.setCategoryId(resultSet.getString("categoryId"));
+        item.setPrice(resultSet.getDouble("price"));
+        return item;
+    }
 }
