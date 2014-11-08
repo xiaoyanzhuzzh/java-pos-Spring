205331c0b951a1878b3c0ac5487d830c4b28c522
zzh <xiaoyanzhuzzh@sina.com>
2014-11-08 10:21:17 +0800

    delete useless code lines

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDaoImpl.java
@@ -1,15 +1,8 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.util.CategoryRowMapper;
-import com.thoughtworks.iamcoach.pos.util.ConnectionUtil;
 import com.thoughtworks.iamcoach.pos.vo.Category;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 
 public class CategoryDaoImpl implements CategoryDao {
     private final SimpleJdbcTemplate simpleJdbcTemplate;
