69f3ddea429d732855bd4cf2b9496b782b759cc3
zzh <xiaoyanzhuzzh@sina.com>
2014-11-08 10:30:40 +0800

    create PromotionRowMapper class

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/PromotionRowMapper.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos.util;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PromotionRowMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+}
