795bba88d22edf540166942524dcfaaddbde2e11
zzh <xiaoyanzhuzzh@sina.com>
2014-11-08 09:17:53 +0800

    delete useless imports

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,6 @@
-import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
-import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImple;
-import com.thoughtworks.iamcoach.pos.dao.ItemDao;
-import com.thoughtworks.iamcoach.pos.dao.ItemDaoImple;
 import com.thoughtworks.iamcoach.pos.service.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 
 public class App {
@@ -16,7 +11,6 @@ public class App {
         ItemService itemServiceImple = (ItemService) context.getBean("itemServiceImple");
 
         Scanner scanner = (Scanner) context.getBean("scanner");
-//        Scanner scanner = new Scanner();
         Cart cart = new Cart(scanner, itemServiceImple);
 
         CategoryService categoryServiceImple = (CategoryServiceImpl) context.getBean("categoryServiceImple");
