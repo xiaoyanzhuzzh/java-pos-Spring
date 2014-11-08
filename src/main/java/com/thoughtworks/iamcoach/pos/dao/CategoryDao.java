16eaf5346b0806479a0fc9c80510f9770286bae1
zzh <xiaoyanzhuzzh@sina.com>
2014-11-08 10:20:06 +0800

    delete useless code lines

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
@@ -2,9 +2,7 @@ package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.vo.Category;
 
-import java.util.List;
-
 public interface CategoryDao {
     Category getCategoryById(String id);
-    List<Category> getCategories();
+//    List<Category> getCategories();
 }
