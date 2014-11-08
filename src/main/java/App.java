9ff6ffa0421347f04274816ef8a23ef12242b847
zzh <xiaoyanzhuzzh@sina.com>
2014-11-08 09:40:33 +0800

    configure printer bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,3 @@
-import com.thoughtworks.iamcoach.pos.service.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,15 +6,7 @@ public class App {
 
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-
-        ItemService itemServiceImple = (ItemService) context.getBean("itemServiceImple");
-
-        Scanner scanner = (Scanner) context.getBean("scanner");
-        Cart cart = new Cart(scanner, itemServiceImple);
-
-        CategoryService categoryServiceImple = (CategoryServiceImpl) context.getBean("categoryServiceImple");
-
-        Printer printer = new Printer(cart, categoryServiceImple );
+        Printer printer = (Printer) context.getBean("printer");
         printer.printAllInfo();
     }
 
